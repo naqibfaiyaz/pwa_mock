110550c2d11572361be312b207ecbf1a7bc85061
Naqib Faiyaz <naqib3110@gmail.com>
2018-10-14 14:59:30 +0600

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,6 +91,17 @@
                     $('#searchBar').addClass('active');
                 }
             });
+
+            $('.multi-item-carousel .item').each(function(){
+                var next = $(this).next();
+                if (!next.length) next = $(this).siblings(':first');
+                next.children(':first-child').clone().appendTo($(this));
+                });
+                $('.multi-item-carousel .item').each(function(){
+                var prev = $(this).prev();
+                if (!prev.length) prev = $(this).siblings(':last');
+                prev.children(':nth-last-child(2)').clone().prependTo($(this));
+            });
         });
     </script>
 </body>
